a5871ed2010057cd83a444a7a6538140ad9e85ec
George Lewis <ghl227@gmail.com>
2023-10-08 10:51:03 -0400

    Fix unit tests

--- src/chess/Fen.test.ts
+++ src/chess/Fen.test.ts
@@ -5,8 +5,8 @@ describe("Chess FEN Parsing", () => {
     const fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     const result = getPieceCounts(fen);
     expect(result).toEqual({
-      white: { p: 8, r: 2, n: 2, b: 2, q: 1, k: 1 },
-      black: { p: 8, r: 2, n: 2, b: 2, q: 1, k: 1 },
+      white: new Map(Object.entries({ p: 8, r: 2, n: 2, b: 2, q: 1, k: 1 })),
+      black: new Map(Object.entries({ p: 8, r: 2, n: 2, b: 2, q: 1, k: 1 })),
     });
   });
 
@@ -14,8 +14,8 @@ describe("Chess FEN Parsing", () => {
     const fen = "8/8/8/8/2P2P2/8/8/8 w - - 0 1";
     const result = getPieceCounts(fen);
     expect(result).toEqual({
-      white: { p: 2, r: 0, n: 0, b: 0, q: 0, k: 0 },
-      black: { p: 0, r: 0, n: 0, b: 0, q: 0, k: 0 },
+      white: new Map(Object.entries({ p: 2, r: 0, n: 0, b: 0, q: 0, k: 0 })),
+      black: new Map(Object.entries({ p: 0, r: 0, n: 0, b: 0, q: 0, k: 0 })),
     });
   });
 });
