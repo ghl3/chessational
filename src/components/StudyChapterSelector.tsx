08dd2b8577c6d7ca15723c697de9a3eada6cc661
George Lewis <ghl227@gmail.com>
2024-02-04 22:02:27 -0500

    Clean up old code

--- src/components/StudyChapterSelector.tsx
+++ src/components/StudyChapterSelector.tsx
@@ -1,8 +1,7 @@
 import { StudyData } from "@/hooks/UseStudyData";
-import React, { useCallback } from "react";
+import React from "react";
 import { ChapterSelector } from "./ChapterSelector";
 import { StudyAdderEditor } from "./StudyAdderEditor";
-import { StudySelector } from "./StudySelector";
 
 interface StudyChapterSelectorProps
   extends React.HTMLAttributes<HTMLDivElement> {
@@ -12,37 +11,14 @@ interface StudyChapterSelectorProps
 export const StudyChapterSelector: React.FC<StudyChapterSelectorProps> = ({
   studyData,
 }) => {
-  const { studies, selectedStudyName, selectedChapterNames } = studyData;
+  const { studies, selectedChapterNames } = studyData;
 
   const chapterNames = studyData.chapters
     ? studyData.chapters.map((chapter) => chapter.name)
     : undefined;
 
-  const onStudyChange = useCallback(
-    (studyName: string) => {
-      studyData.selectStudy(studyName);
-    },
-    [studyData],
-  );
-
-  const onStudyDelete = useCallback(
-    (studyName: string) => {
-      studyData.removeStudy(studyName);
-    },
-    [studyData],
-  );
-
   return (
     <div className="flex flex-row space-x-4 items-center">
-      {studies && studies.length > 0 ? (
-        <StudySelector
-          studies={studies}
-          selectedStudy={selectedStudyName}
-          onStudyChange={onStudyChange}
-          onStudyDelete={onStudyDelete}
-        />
-      ) : null}
-
       <StudyAdderEditor
         studies={studies}
         selectedStudy={studyData.selectedStudy || null}
